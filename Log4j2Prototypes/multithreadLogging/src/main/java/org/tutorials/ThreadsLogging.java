1914b11c794da664e980b0f446cbdd07d5e76ea8
rarecasescenario <rare.case.scenario@gmail.com>
2023-04-21 03:46:31 -0400

    added multiple loggers

--- Log4j2Prototypes/multithreadLogging/src/main/java/org/tutorials/ThreadsLogging.java
+++ Log4j2Prototypes/multithreadLogging/src/main/java/org/tutorials/ThreadsLogging.java
@@ -6,13 +6,18 @@ import org.apache.logging.log4j.Logger;
 public class ThreadsLogging {
 
 	private static Logger logger = LogManager.getLogger(ThreadsLogging.class);
-
+	private static Logger dataLogger = LogManager.getLogger("data-log");
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Dude");
 		logger.debug("debug message");
 		logger.info("info message");
 		logger.error("error message");
+		
+		dataLogger.debug("data debug message");
+		dataLogger.info("data info message");
+		dataLogger.error("data error message");
 	}
 
 }
